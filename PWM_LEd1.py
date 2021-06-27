f8d5b94ba7b4ffe4c3476f89a3cf1b322e14cecf
Imre Gergely Olah <ogi.hrrf@gmail.com>
2021-06-27 15:14:48 +0100

    Refactoring

--- PWM_LEd1.py
+++ PWM_LEd1.py
@@ -5,15 +5,15 @@ led1 = io.PWMLED(25)
 led2 = io.LED(8)
 ldr = io.LightSensor(15)
 
-led2.source = ldr.value
+led2.source = ldr
 
 while True:
     for i in range(5):
         led1.value = i/10
-        print (i)
+        print (i, ldr.value)
         sleep(0.3)
 
     for i in range(3,0, -1):
         led1.value = i/10
-        print (i)
+        print (i, ldr.value)
         sleep(0.3)
