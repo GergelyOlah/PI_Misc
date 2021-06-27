281189ce588db92fd5cb6c31d482f74f0944b197
GergelyOlah <olah.gergely.imre@gmail.com>
2021-06-27 15:08:16 +0100

    LDR added to PWMLED

--- PWM_LEd1.py
+++ PWM_LEd1.py
@@ -1,15 +1,19 @@
 import gpiozero as io
 from time import sleep
 
-led = io.PWMLED(25)
+led1 = io.PWMLED(25)
+led2 = io.LED(8)
+ldr = io.LightSensor(15)
+
+led2.source = ldr.value
 
 while True:
     for i in range(5):
-        led.value = i/10
+        led1.value = i/10
         print (i)
         sleep(0.3)
 
     for i in range(3,0, -1):
-        led.value = i/10
+        led1.value = i/10
         print (i)
         sleep(0.3)
