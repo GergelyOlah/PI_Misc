8cd52f385c67b4cdf96711f7526c4d5c60db5709
Imre Gergely Olah <ogi.hrrf@gmail.com>
2021-06-27 15:01:17 +0100

    PWMLED adjusted

--- PWM_LEd1.py
+++ PWM_LEd1.py
@@ -1,7 +1,15 @@
 import gpiozero as io
+from time import sleep
 
-led = io.PWMLED(4)
+led = io.PWMLED(25)
 
 while True:
     for i in range(5):
-        led.value(i)
+        led.value = i/10
+        print (i)
+        sleep(0.3)
+
+    for i in range(3,0, -1):
+        led.value = i/10
+        print (i)
+        sleep(0.3)
